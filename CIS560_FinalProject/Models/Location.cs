b5490885f3105970f92fda2479137549724d4076
Zach <zmironman@gmail.com>
2018-11-06 17:43:31 -0600

    Added Location to Models

---
+++ CIS560_FinalProject/Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CIS560_FinalProject.Models
+{
+    public class Location
+    {
+        //LocationID is automatically setup as PK
+        public int LocationID { get; set; }
+
+        public string Venue { get; set; }
+
+        public string City { get; set; }
+
+        public string StateProvince { get; set; }
+    }
+}
