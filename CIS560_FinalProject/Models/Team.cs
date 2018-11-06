0e9268e367ace54c881a97a447778d565d2d33ca
Brock Carpani <brockcarpani@ksu.edu>
2018-11-06 17:40:33 -0600

    Added team class

---
+++ CIS560_FinalProject/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS560_FinalProject.Models
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+
+        public int LocationID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Mascot { get; set; }
+    }
+}
