820a378632772adbc37393fcf8b5c4e976c66827
dakweezy <dakweezy@ksu.edu>
2018-11-06 17:35:40 -0600

    Made Player.cs

---
+++ CIS560_FinalProject/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS560_FinalProject.Models
+{
+    public class Player
+    {
+        //PlayerID is automatically setup as PK?
+        public int PlayerID { get; set; }
+
+        public int TeamID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Number { get; set; }
+
+
+    }
+}
