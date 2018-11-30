6b0a0a5e940b18123d080aea3d98d38c3c3a39d4
Mitch Gehrt <mjgehrt@ksu.edu>
2018-11-30 10:09:36 -0600

    Recreated database without Entity Framework, implemented CRUD for Tournaments using StoredProcedures

---
+++ CIS560_FinalProject/SQL/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].Locations
+(
+    LocationID    INT            IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    Venue         NVARCHAR (MAX) NULL,
+    City          NVARCHAR (MAX) NULL,
+    StateProvince NVARCHAR (MAX) NULL,
+);
+
