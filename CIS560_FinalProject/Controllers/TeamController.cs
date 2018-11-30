4b449fb82aca46ebec89403a6827c2bbb83e6540
Jacob Victor <successparrots@gmail.com>
2018-11-30 13:00:47 -0600

    Procedure

--- CIS560_FinalProject/Controllers/TeamController.cs
+++ CIS560_FinalProject/Controllers/TeamController.cs
@@ -19,13 +19,31 @@ namespace CIS560_FinalProject.Controllers
         {
             //would like to use sql instead of other stuff
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection cnn = new SqlConnection(connectionString);
-            try
+            string queryString = "SELECT * FROM dbo.Tournaments";
+            using (SqlConnection cnn = new SqlConnection(connectionString))
             {
-                cnn.Open();
+                try
+                {
+                    cnn.Open();
+                    SqlCommand sqlCommand = new SqlCommand(queryString, cnn);
+                    using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        var list = new List<Tournament>();
+                        while (dataReader.Read())
+                        {
+                            var name = dataReader["Name"];
+                            var sport = dataReader["Sport"];
+                            var id = dataReader["TournamentID"];
+                        }
+                    }
+                }
+                catch (Exception ee)
+                {
+
+                }
+
+                return View();
             }
-            
-            return View();
         }
 
         public ActionResult Create()
