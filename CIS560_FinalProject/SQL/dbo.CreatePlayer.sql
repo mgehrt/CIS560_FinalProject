a15f5a87de8cb142a7524eb7dc057bb6c06504ea
Jacob Victor <jrvictor@WIN2.CS.KSU.EDU>
2018-12-04 20:55:03 -0600

    Added ability to add players on main player page

---
+++ CIS560_FinalProject/SQL/dbo.CreatePlayer.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CreatePlayer]
+	@FirstName NVARCHAR(128),
+	@LastName NVARCHAR(128),
+	@TeamName NVARCHAR(128),
+	@Number INT
+AS
+	INSERT INTO Players (FirstName, LastName, TeamID, Number)
+	SELECT @FirstName, @LastName, T.TeamID, @Number
+	FROM Teams T
+	WHERE T.[Name] = @TeamName
+RETURN 0
